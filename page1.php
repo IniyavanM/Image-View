ab7a9244d6820e5fbbb94af92611b4dcddb961a1
IniyavanM <56250683+IniyavanM@users.noreply.github.com>
2019-10-14 17:09:42 +0530

    Add files via upload

---
+++ page1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Think Twise Code Once</title>
+	<link rel="stylesheet" type="text/css" href="style1.css">
+</head>
+<body>
+	<div class="container">
+		<img src="image1.jpg" alt="image1" style="width:100%";>
+		<div class="top-centerded">Think Twise</br>Code Once</div>
+		<div class="paragraph"><p>In this site was developed by iniyavan</br>This is a simple reference site</p></div>
+		<div class="iframe">
+			<iframe src="page2.php" height="850" width="1050"></iframe>
+		</div>
+		<div class="quotes">Some Inspirational Quotes</div>
+		<div class="steve-jobs"><b>Steve Jobs</b></br><p>“Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do. If you haven’t found it yet, keep looking. Don’t settle. As with all matters of the heart, you’ll know when you find it.”</p></div>
+		<div class="sundar-pichai"><b>Sundar Pichai</b></br><p>I do think it’s important to follow your dreams and do something which you are excited by. If you follow your heart and do what you like, you will always do much better. It doesn’t matter what your educational qualification is.”</p></div>
+		<div class="mark-zuckerberg"><b>Mark Zuckerberg</b></br><p>“Our society needs more heroes who are scientists, researchers, and engineers. We need to celebrate and reward the people who cure diseases, expand our understanding of humanity and work to improve people’s lives.”</p></div>
+		<div class="bill-gates"><b>Bill Gates</b></br><p>If all my bridge coach ever told me was that I was ‘satisfactory,’ I would have no hope of ever getting better. How would I know who was the best? How would I know what I was doing differently?</p></div>
+	</div>
+	<footer>
+		<p>
+			Posted by: IniyavanM
+		</p>
+		<p>
+			Contact information: <a href="www.inichocos@gmail.com">inichocos@gmail.com</a></p>
+		</p>
+	</footer>
+</body>
+</html>
---
+++ page2.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Top 6 programming Languages</title>
+	<link rel="stylesheet" type="text/css" href="style2.css">
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<script type="text/javascript">
+			function showMessage(){
+				alert("Hi Mr/Mrs.");
+			}
+		</script>
+</head>
+<body>
+	<div class="h1"><h1>Welcome Mr/Mrs.Coder</h1></div>
+	<div class="p"><p>Top 6 programming languages</p></div>
+	<div class="image2">
+	<img src="image2.jpg" alt="Snow" style="width:100%"></div>
+	<div class="p1"><p>The programming Languages are.....</p></div>
+	<div class="table">
+		<table>
+			<tr>
+				<td>
+					<img src="c.png" alt="C" style="width: 50%">
+					<input type="button" class="btn btn-primary" id="btnShowMsg" value="More info about C" onclick="showMessage()"/>
+				</td>
+				<td>
+					<img src="c++.png" alt="C++" style="width: 50%">
+					<input type="button" class="btn btn-primary" id="btnShowMsg" value="More info about C++" onclick="showMessage()"/>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<img src="cc.png" alt="C#" style="width: 50%">
+					<input type="button" class="btn btn-primary" id="btnShowMsg" value="More info about C" onclick="showMessage()"/>
+				</td>
+				<td>
+					<img src="java.png" alt="Java" style="width: 50%">
+					<input type="button" class="btn btn-primary" id="btnShowMsg" value="More info about C++" onclick="showMessage()"/>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<img src="python.png" alt="Python" style="width: 50%">
+					<input type="button" class="btn btn-primary" id="btnShowMsg" value="More info about C" onclick="showMessage()"/>
+				</td>
+				<td>
+					<img src="Ruby.png" alt="Ruby" style="width: 50%">
+					<input type="button" class="btn btn-primary" id="btnShowMsg" value="More info about C++" onclick="showMessage()"/>
+				</td>
+			</tr>
+		</table>
+	</div>
+</body>
+</html>
